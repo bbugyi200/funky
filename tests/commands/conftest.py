528dbc72c5e660c60d6c452d315a105ade2f55df
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-05-27 20:00:03 -0400

    Ref: refactored tests

--- tests/commands/conftest.py
+++ tests/commands/conftest.py
@@ -15,17 +15,16 @@ def cleandir():
     os.chdir(newpath)
 
 
-alias_dict = {'multiline': 'echo "Hello"\necho "world!"',
-              'run': 'python main.py',
-              'T': 'pytest test_main.py'}
-
-show_expected = {'run': 'run() {{ {0}; }}\n'.format(alias_dict['run']),
-                 'T': 'T() {{ {0}; }}\n'.format(alias_dict['T']),
-                 'multiline': 'multiline() {{\n\t{0}\n}}\n'.format(alias_dict['multiline'].replace('\n', '\n\t'))}
+@pytest.fixture
+def alias_dict():
+    alias_dict = {'multiline': 'echo "Hello"\necho "world!"',
+                  'run': 'python main.py',
+                  'T': 'pytest test_main.py'}
+    return alias_dict
 
 
 @pytest.fixture
-def local_db():
+def fake_db(alias_dict):
     """Setup/teardown a local alias database."""
     filename = commands.Command.LOCALALIAS_DB_FILENAME
     with open(filename, 'w') as f:
@@ -35,12 +34,12 @@ def local_db():
 
 
 @pytest.fixture(params=[
-    ('run', False, show_expected['run']),
-    ('multiline', False, show_expected['multiline']),
-    (None, False, '{0}\n{1}\n{2}'.format(show_expected['T'], show_expected['multiline'], show_expected['run']))
+    ('run', False),
+    ('multiline', False),
+    (None, False)
 ], ids=['run', 'multiline', 'None'])
 def cmd_args(request):
     """Returns a named tuple of command arguments and expected results."""
-    Args = collections.namedtuple('Args', ['alias', 'color', 'expected'])
-    args = Args(request.param[0], request.param[1], request.param[2])
+    Args = collections.namedtuple('Args', ['alias', 'color'])
+    args = Args(request.param[0], request.param[1])
     return args
--- tests/commands/test_show.py
+++ tests/commands/test_show.py
@@ -1,4 +1,4 @@
-"""Tests command functionality."""
+"""Tests for the 'show' command."""
 
 import unittest.mock as mock
 
@@ -10,21 +10,37 @@ pytestmark = pytest.mark.usefixtures("debug_mode")
 
 
 @pytest.fixture
-def show_cmd(cmd_args):
+def show_expected(alias_dict):
+    show_expected = {
+        'run': 'run() {{ {0}; }}\n'.format(alias_dict['run']),
+        'T': 'T() {{ {0}; }}\n'.format(alias_dict['T']),
+        'multiline': 'multiline() {{\n\t{0}\n}}\n'.format(alias_dict['multiline'].replace('\n', '\n\t'))
+    }
+
+    return show_expected
+
+
+@pytest.fixture
+def show_cmd(cmd_args, show_expected):
     """Builds and returns show command."""
-    show_cmd = commands.Show(cmd_args.alias, color=cmd_args.color)
-    show_cmd.expected = cmd_args.expected
-    return show_cmd
+    cmd = commands.Show(cmd_args.alias, color=cmd_args.color)
+    if cmd_args.alias is None:
+        cmd.expected = '{0}\n{1}\n{2}'.format(show_expected['T'],
+                                              show_expected['multiline'],
+                                              show_expected['run'])
+    else:
+        cmd.expected = show_expected[cmd_args.alias]
+    return cmd
 
 
-def test_show_command(capsys, cleandir, local_db, show_cmd):
+def test_show(capsys, cleandir, fake_db, show_cmd):
     """Tests show command."""
     show_cmd()
     captured = capsys.readouterr()
     assert captured.out == show_cmd.expected
 
 
-def test_show_command_failure(cleandir, show_cmd):
+def test_show_failure(cleandir, show_cmd):
     """Tests show command fails properly when no local alias database exists."""
     with pytest.raises(RuntimeError):
         show_cmd()
