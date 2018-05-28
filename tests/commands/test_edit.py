3c1f92529e1d107604718625fcaa3e46a2664a4a
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-05-28 15:13:58 -0400

    Add: test_edit_all

--- tests/commands/test_edit.py
+++ tests/commands/test_edit.py
@@ -20,8 +20,24 @@ def edit_cmd(cmd_args):
 
 @mock.patch('localalias.commands.Edit.edit_alias')
 def test_edit(edit_alias, cleandir, fake_db, edit_cmd):
+    """Tests edit command."""
     edited_cmd_string = 'TEST COMMAND STRING'
     edit_alias.return_value = edited_cmd_string
     edit_cmd()
     loaded_aliases = shared.load_aliases()
     assert loaded_aliases[edit_cmd.alias] == edited_cmd_string
+
+
+@mock.patch('localalias.commands.Edit.edit_alias')
+def test_edit_all(edit_alias, cleandir, fake_db, alias_dict):
+    """Tests edit command when no specific alias is given."""
+    def new_cmd_string(old):
+        return '{}: Edited Command String'.format(old)
+
+    edit_alias.side_effect = new_cmd_string
+    edit_cmd = commands.Edit(None)
+    edit_cmd()
+
+    loaded_aliases = shared.load_aliases()
+    for alias in alias_dict:
+        assert loaded_aliases[alias] == new_cmd_string(alias)
