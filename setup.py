c3f10be1bd86022240fa552a737a2c53c9602d5c
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-05-29 22:06:36 -0400

    Meta: update release

--- setup.py
+++ setup.py
@@ -11,39 +11,36 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = []
-
-setup_requirements = ['pytest-runner', ]
-
-test_requirements = ['pytest', ]
-
 setup(
     author="Bryan M Bugyi",
     author_email='bryanbugyi34@gmail.com',
+    install_requires=['pygments'],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     description="A light-weight shell wrapper that allows you to create per-directoy command aliases.",
-    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'localalias = localalias.app:main',
+            'la = localalias.app:main'
+        ]
+    },
     license="MIT license",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='localalias',
     name='localalias',
-    packages=find_packages(include=['localalias']),
-    setup_requires=setup_requirements,
+    packages=find_packages(include=['localalias', 'localalias.utils']),
     test_suite='tests',
-    tests_require=test_requirements,
+    tests_require=['pytest'],
     url='https://github.com/bbugyi200/localalias',
-    version='2.0',
+    version='2.0.0',
     zip_safe=False,
 )
