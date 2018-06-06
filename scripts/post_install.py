802490fd34de1da5a2cd85b07747b6f1df111e7b
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-06-06 13:47:23 -0400

    Mod: create symlink to localalias.plugin.zsh instead of using copy (#33)

--- scripts/post_install.py
+++ scripts/post_install.py
@@ -3,17 +3,22 @@
 import errno
 import getpass
 import os
-import shutil
+
+_package_dir = os.path.dirname(os.path.realpath(__file__))
+_user = getpass.getuser()
 
 
 def run():
-    package_dir = os.path.dirname(os.path.realpath(__file__))
-    _install_zsh_plugin(package_dir)
+    _install_omz_plugin()
 
 
-def _install_zsh_plugin(package_dir):
-    """Copys zsh plugin to oh-my-zsh plugin dir (if oh-my-zsh is installed)."""
-    zsh_custom_dirs = ['/home/{}/.oh-my-zsh/custom'.format(getpass.getuser()),
+def _install_omz_plugin():
+    """Install oh-my-zsh Plugin
+
+    Creates symlink from localalias shell extension zsh plugin in oh-my-zsh plugin dir
+    (if oh-my-zsh is installed).
+    """
+    zsh_custom_dirs = ['/home/{}/.oh-my-zsh/custom'.format(_user),
                        '/usr/share/oh-my-zsh/custom']
 
     ohmyzsh_dir = None
@@ -27,9 +32,15 @@ def _install_zsh_plugin(package_dir):
 
     _create_dir(ohmyzsh_dir + '/plugins/localalias')
 
-    src = '{}/localalias/shell/localalias.sh'.format(os.path.dirname(package_dir))
+    src = '{}/localalias/shell/localalias.sh'.format(os.path.dirname(_package_dir))
     dest = '{}/plugins/localalias/{}'.format(ohmyzsh_dir, 'localalias.plugin.zsh')
-    shutil.copyfile(src, dest)
+
+    try:
+        os.unlink(dest)
+    except FileNotFoundError as e:
+        pass
+
+    os.symlink(src, dest)
 
 
 def _create_dir(directory):
