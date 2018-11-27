b9f2aa538ea0dc21a8d6ba8d904476a4bf607b29
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-11-27 17:47:44 -0500

    Tests: add project directory to PATH

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
-import pytest
+import os
+import sys
+
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_dir)
+
+import pytest  # noqa
 
 
 if __name__ == "__main__":
-    pytest.main()
+    sys.exit(pytest.main())
