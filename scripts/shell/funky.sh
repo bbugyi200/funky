c424632722d3dcd6950c0d561891f77137f2335c
Bryan Bugyi <bryanbugyi34@gmail.com>
2020-12-12 17:35:34 -0500

    [164] Started to integrate @french-frie's changes to funky.sh

--- scripts/shell/funky.sh
+++ scripts/shell/funky.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-#########################################
-#  funky Shell Integration Script       #
-#########################################
-
-
-##### WHAT DOES THIS SCRIPT DO?
-# - Source local and global funks on startup.
-# - Source local funks everytime the directory changes.
-# - Define two wrapper functions, `funky` and `gfunky`.
-
-##### INSTALLATION
+##############################################################################
+#  funky Shell Integration Script
+#
+# ----- WHAT DOES THIS SCRIPT DO?
+# * Source local and global funks on startup.
+# * Source local funks everytime the directory changes.
+# * Define two wrapper functions, `funky` and `gfunky`.
+#
+# ----- INSTALLATION
 # In order to take advantage of the full benefits of funky, this script must
 # be sourced directly into your .zshrc file. See the official docs for more
 # information:
-# 
-# https://github.com/bbugyi200/funky#additional-installation-steps
+#
+#     https://github.com/bbugyi200/funky#additional-installation-steps
+##############################################################################
 
-##### Create temporary funky directory
-# ensure running as root
+# TODO(bbugyi): Add tests for new behavior.
+# TODO(bbugyi): Make funky development environment easier to setup.
+# TODO(bbugyi): Allow funky to be installed via -e.
+# TODO(bbugyi): Add Dockerfile to funky.
+
+# The FUNKY_CMD envvar must be overridable (e.g. by test_funky.sh).
+FUNKY_CMD="${FUNKY_CMD:="$(command -v funky)"}"
+
+##### CREATE FUNKY DATA DIRECTORY
 if [ "$EUID" -eq 0 ]; then
     _home_dir=/root
 else
@@ -33,70 +39,107 @@ fi
 
 [[ -d "$_xdg_data_dir" ]] || mkdir -p "$_xdg_data_dir"
 
-##### Function used for sourcing funks
-
-FUNKY_CMD="${FUNKY_CMD:="$(command -v funky)"}"
-
+##### HELPER FUNCTIONS
+# Source all global funks.
 _source_globals() { source <(${FUNKY_CMD} --verbose --global); }
+
+# Source local (per-directory) funks.
 _source_locals() { source <(${FUNKY_CMD} --verbose); }
 
-_maybe_source_locals() {
-    if [[ -f "$PWD"/.funky ]]; then
-        _source_locals
+# Get the current TTY number.
+_tty_number() { tty | sed 's/[^0-9]*//'; }
+
+# Save PWD and associated funks to disk.
+_save_locals() {
+    export _ACTIVE_LOCALPATH="$_xdg_data_dir"/localpath-"$(_tty_number)"
+    export _ACTIVE_ALIASES="$_xdg_data_dir"/localalias-"$(_tty_number)"
+
+    ${FUNKY_CMD} | sed -E 's/\(.+$//' >"$_ACTIVE_ALIASES"
+
+    if [[ ! -f "$_ACTIVE_LOCALPATH" ]]; then
+        echo "$PWD" >"$_ACTIVE_LOCALPATH"
     fi
 }
 
+# Unset funks from last directory.
+_unset_locals() {
+    source <(sed 's/^/unset -f /' "${_ACTIVE_ALIASES}")
+    command rm -f "$_ACTIVE_ALIASES"
+    unset _ACTIVE_ALIASES
+}
+
+# Activate local funks, if necessary.
+_maybe_source_locals() {
+    if [[ -f "$PWD"/.funky ]]; then
+        _source_locals
+        if [[ "$PWD" != "$_home_dir" ]]; then
+            _save_locals
+        fi
+    fi
+}
+
+# Activate global funks, if necessary.
 _maybe_source_globals() {
     if [[ -f "${_home_dir}"/.funky ]]; then
         _source_globals
     fi
 }
 
-##### Source funks on startup
+# Unset old local funks, if necessary.
+_maybe_unset_locals() {
+    if [[ -f "$OLDPWD"/.funky ]] && [[ -f "$_ACTIVE_ALIASES" ]]; then
+        _unset_locals
+    fi
+}
+
+##### SOURCE FUNKS ON STARTUP
 _maybe_source_globals
 _maybe_source_locals
 
-##### Source appropriate funks everytime the directory is changed
+##### SOURCE APPROPRIATE FUNKS EVERYTIME THE DIRECTORY IS CHANGED
 #
-# - Is run everytime the directory is changed.
-# - Lazily loads global funks and local funks while attempting to maintain parent's local
+# * Is run everytime the directory is changed.
+# * Lazily loads global funks and local funks while attempting to maintain parent's local
 #   funks.
 chpwd() {
-    if [[ -f "$_xdg_data_dir"/localpath ]] && [[ "$PWD" != "$(cat "$_xdg_data_dir"/localpath)/"* ]]; then
+    if [[ -f "$_ACTIVE_LOCALPATH" ]] && ! [[ "$PWD" =~ $(cat "$_ACTIVE_LOCALPATH")/? ]]; then
         _maybe_source_globals
-        command rm -f "$_xdg_data_dir"/localpath
+        _maybe_unset_locals
+        command rm -f "$_ACTIVE_LOCALPATH"
+        unset _ACTIVE_LOCALPATH
     fi
 
     if [[ "$PWD" != "$_home_dir" ]] && [[ -f "$PWD"/.funky ]]; then
-        _source_locals
-        if [[ ! -f $_xdg_data_dir/localpath ]]; then
-            echo "$PWD" > "$_xdg_data_dir"/localpath
+        _maybe_source_locals
+        if [[ ! -f "$_ACTIVE_LOCALPATH" ]]; then
+            echo "$PWD" >"$_ACTIVE_LOCALPATH"
         fi
     fi
 }
 
 PROMPT_COMMAND=chpwd
 
-##### Wrapper used to interact with local funks
-unalias funky &> /dev/null
-
+##### FUNKY'S WRAPPER FUNCTIONS
+# Wrapper used to interact with local funks.
+unalias funky &>/dev/null
 funky() {
     touch "$_xdg_data_dir"/timestamp
 
     ${FUNKY_CMD} --color=y "$@"
     if [[ .funky -nt "$_xdg_data_dir"/timestamp ]]; then
+        _maybe_unset_locals
         _source_locals
     fi
 }
 
-##### Wrapper used to interact with global funks
-unalias gfunky &> /dev/null
-
+# Wrapper used to interact with global funks.
+unalias gfunky &>/dev/null
 gfunky() {
     touch "$_xdg_data_dir"/timestamp
     ${FUNKY_CMD} --global --color=y "$@"
     if [[ ~/.funky -nt "$_xdg_data_dir"/timestamp ]]; then
         _source_globals
+        _maybe_unset_locals
         _maybe_source_locals
     fi
 }
--- tests/scripts/shell/test_funky.sh
+++ tests/scripts/shell/test_funky.sh
@@ -5,7 +5,8 @@ export FUNKY_CMD="python ${PWD}/funky"
 
 source ./scripts/shell/funky.sh 2> /dev/null
 
-oneTimeSetUp() { 
+oneTimeSetUp() {
+    mkdir -p "${HOME}"
     mkdir -p /tmp/A/B
     echo '{"my_funk": "echo my_funk_def", "my_override_funk": "echo is_local"}' > /tmp/A/.funky
     echo '{"my_global_funk": "echo my_global_funk_def", "my_override_funk": "echo is_global"}' > "${HOME}"/.funky
@@ -22,6 +23,19 @@ oneTimeTearDown() {
 setUp() { :; }
 tearDown() { :; }
 
+function assert_funk_defined() { _assert_funk "$1" 0; }
+function assert_funk_not_defined() { _assert_funk "$1" 1; }
+function _assert_funk() {
+    local funk_name="$1"
+    shift
+
+    local ec="$1"
+    shift
+
+    type "${funk_name}" > /dev/null
+    assertEquals "${ec}" "$?"
+}
+
 ###################################################################
 #  Maybe Source Tests                                             #
 ###################################################################
@@ -30,16 +44,14 @@ test_maybe_source_locals() {
     _maybe_source_locals
     popd &> /dev/null || return 1
 
-    type my_funk > /dev/null
-    assertEquals 0 "$?"
+    assert_funk_defined "my_funk"
     assertEquals "my_funk_def" "$(my_funk)"
 }
 
 test_maybe_source_globals() {
     _maybe_source_globals
 
-    type my_global_funk > /dev/null
-    assertEquals 0 "$?"
+    assert_funk_defined "my_global_funk"
     assertEquals "my_global_funk_def" "$(my_global_funk)"
 }
 
@@ -47,21 +59,30 @@ test_maybe_source_globals() {
 #  chpwd() Function Tests                                         #
 ###################################################################
 test_chpwd() {
+    local tty_number="$(tty | sed 's/[^0-9]*//')"
+    local localpath="localpath-${tty_number}"
+
     cd /tmp/A || return 1
     chpwd
     assertEquals "is_local" "$(my_override_funk)"
-    assertEquals "/tmp/A" "$(cat /tmp/home/.local/share/funky/localpath)"
+    assertEquals "/tmp/A" "$(cat /tmp/home/.local/share/funky/"${localpath}")"
+
+    assert_funk_defined "my_funk"
 
     cd /tmp/A/B || return 1
     chpwd
     assertEquals "is_local" "$(my_override_funk)"
-    assertEquals "/tmp/A" "$(cat /tmp/home/.local/share/funky/localpath)"
+    assertEquals "/tmp/A" "$(cat /tmp/home/.local/share/funky/"${localpath}")"
+
+    assert_funk_defined "my_funk"
 
     cd /tmp || return 1
     chpwd
     assertEquals "is_global" "$(my_override_funk)"
-    test -f /tmp/home/.local/share/funky/localpath
+    test -f /tmp/home/.local/share/funky/"${localpath}"
     assertEquals 1 "$?"
+
+    assert_funk_not_defined "my_funk"
 }
 
 
