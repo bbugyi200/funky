5190e25982e1498175c974c6e609a922879897ae
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-08-21 17:47:40 -0400

    Bump version: 3.5.0 → 3.5.1

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite="tests",
     tests_require=tests_require(),
     url="https://github.com/bbugyi200/funky",
-    version="3.5.0",
+    version="3.5.1",
     zip_safe=False,
 )
